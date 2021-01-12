3df959d3f608723a6224c6edd710acce691aaf23
Ask0nn <Askonzir@gmail.com>
2021-01-13 01:25:46 +0300

    Выполнение задания

--- src/com/ask0n/Main.java
+++ src/com/ask0n/Main.java
@@ -3,6 +3,12 @@ package com.ask0n;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b); //Нельзя делить на ноль
+
+        calc.println.accept(c);
     }
 }
